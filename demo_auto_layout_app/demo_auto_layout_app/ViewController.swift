0f5dd9e5838455462b0bc2a2deb377c306ee14ef
ramSde <techmacchu@gmail.com>
2024-02-14 17:51:36 +0530

    calculaotor app ui

---
+++ demo_auto_layout_app/demo_auto_layout_app/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  demo_auto_layout_app
+//
+//  Created by Zignuts Tchnolab on 14/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        // Do any additional setup after loading the view.
+    }
+  
+    
+  
+
+}
+
