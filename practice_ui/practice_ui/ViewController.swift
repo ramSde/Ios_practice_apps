f9fd9606530d57161313268b4dfd198dfc58e0b2
ramSde <techmacchu@gmail.com>
2024-02-19 10:16:13 +0530

    login screen app

---
+++ practice_ui/practice_ui/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  practice_ui
+//
+//  Created by Zignuts Tchnolab on 16/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
