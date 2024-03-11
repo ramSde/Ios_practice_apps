e5f287959009eff5244ab8b80315d85ef0c02b26
ramSde <techmacchu@gmail.com>
2024-03-11 18:58:30 +0530

    90% homescreen bent box

---
+++ BentBoxHomeScreen/BentBoxHomeScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BentBoxHomeScreen
+//
+//  Created by Zignuts Tchnolab on 11/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
